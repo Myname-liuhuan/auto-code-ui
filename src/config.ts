72536eb8927cf214589a22303d461175093ec82e
Henry <studyliuhuan@163.com>
2025-04-24 15:16:36 +0800

    refactor: 配置调整

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,8 @@
 // 应用公共配置
 export const AppConfig = {
-<<<<<<< HEAD
   // nodejs的地址和端口
   host: '127.0.0.1',
   port: 9010,
-=======
-  // 开发服务器端口
-  devPort: 9010,
-  // 开发服务器地址
-  devServerUrl: 'http://127.0.0.1:9010',
-  // 生产环境API地址
-  apiBaseUrl: 'https://api.xxx.com',
->>>>>>> c7cfedbb211c9d4fdda2563dd4c6e79e6ee864f4
   //后端服务地址
   targetUrl: 'http://127.0.0.1:8089'
 };
