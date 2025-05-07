ca231ec15073586a5dea4e6ee3edadf33662ae9d
Henry <studyliuhuan@163.com>
2025-05-07 14:28:42 +0800

    config： 使用配置文件区分环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,36 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, type PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+  const plugins: PluginOption[] = [vue()]
+  if (env.VITE_APP_ENV === 'dev') {
+    plugins.push(vueDevTools() as PluginOption)
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  optimizeDeps: {
-    exclude: ['vite-plugin-vue-devtools']
-  },
-  server: {
-    host:'0.0.0.0',
-    port: 9010,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8089',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+    optimizeDeps: {
+      exclude: ['vite-plugin-vue-devtools']
+    },
+    server: {
+      host:'0.0.0.0',
+      port: 9010,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
       }
     }
   }
